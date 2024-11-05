6e16dcd3d7ef8de5834fb29ded5a2453cceded2e
ShiromaHajime <hajishiro.hs@gmail.com>
2024-11-05 20:32:35 -0300

    [fix]: agrego provider que quité por error

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,40 @@
-import { Stack } from "expo-router";
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect } from 'react';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+import { useAuth0, Auth0Provider } from 'react-native-auth0';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <Auth0Provider domain="dev-fvp00ztww2h1bogg.us.auth0.com" clientId="gCPmmvZb7eB87YTcbQomLBwHpaVPq869">
+      <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="index" options={{ headerShown: false }} />
+        </Stack>
+      </ThemeProvider>
+    </Auth0Provider>
   );
 }
