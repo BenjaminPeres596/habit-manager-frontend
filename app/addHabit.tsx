d7cd6bba6a6875b7dbc727fb5bf03933e726401f
ShiromaHajime <hajishiro.hs@gmail.com>
2024-11-27 10:43:37 -0300

    feat: integrate habit context in addHabit screen to handle new habit creation

--- app/addHabit.tsx
+++ app/addHabit.tsx
@@ -1,10 +1,28 @@
+import { habitListContext } from '@/context/habitContext';
+import { useContext } from 'react';
 import { Text, View, StyleSheet, Button } from 'react-native';
 
-function handleNewHabit() {
-  console.log('Add habit button pressed');
-}
 
 export default function addTaskScreen() {
+  const context = useContext(habitListContext);
+
+  if (!context) {
+    throw new Error('useHabitList must be used within a HabitListProvider');
+  }
+
+  const { habits, setHabits } = context;
+
+  function handleNewHabit() {
+    console.log('Add habit button pressed');
+    const newHabit = {
+      id: habits.length + 1,
+      name: 'New Habit',
+      priority: 'Low' as 'Low',
+      completed: false,
+    };
+    setHabits([...habits, newHabit]);
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>add task screen</Text>
