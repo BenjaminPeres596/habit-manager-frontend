ef60208dcfec3594b4b41baf7d1044ce26275db8
ShiromaHajime <hajishiro.hs@gmail.com>
2024-11-27 10:33:28 -0300

    feat: add delete functionality to TaskDetailsScreen and navigate to home on deletion

--- app/taskDetails.tsx
+++ app/taskDetails.tsx
@@ -1,9 +1,11 @@
-import { Text, View, StyleSheet } from 'react-native';
-import { useLocalSearchParams } from 'expo-router';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { useLocalSearchParams, useRouter } from 'expo-router';
 import { useContext } from 'react';
 import { habitListContext } from '../context/habitContext'; // Adjust the import path as needed
 
 export default function TaskDetailsScreen() {
+  const navigation = useRouter();
+
   const { id } = useLocalSearchParams(); // Access the habit ID from the query parameters
 
   const context = useContext(habitListContext);
@@ -15,6 +17,16 @@ export default function TaskDetailsScreen() {
   const { habits, setHabits } = context;
 
   const habit = habits.find(h => h.id === Number(id)); // Find the habit with the matching ID
+
+  function handleDelete() {
+    // Implement the delete logic here
+    console.log('Delete button pressed');
+    if (habit) {
+      setHabits(habits.filter(h => h.id !== habit.id));
+      navigation.navigate("/home")
+    }
+    
+  }
 
   if (!habit) {
     return (
@@ -31,6 +43,7 @@ export default function TaskDetailsScreen() {
       <Text style={styles.text}>Name: {habit.name}</Text>
       <Text style={styles.text}>Priority: {habit.priority}</Text>
       <Text style={styles.text}>Completed: {habit.completed ? 'Yes' : 'No'}</Text>
+      <Button title="eliminar" onPress={handleDelete}></Button>
     </View>
   );
 }
